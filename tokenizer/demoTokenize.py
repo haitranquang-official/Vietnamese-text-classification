686a5855783021637e609926d71af17b8cd9cdce
Minh Triết <swaggiehippdude@gmail.com>
2022-06-10 12:47:52 +0700

    create dict token1 token2 token3

--- tokenizer/demoTokenize.py
+++ tokenizer/demoTokenize.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2 import token
 from turtle import clear
 from matplotlib.pyplot import get
 import mysql.connector
@@ -10,9 +11,24 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
-
+tokens = {}
 mycursor.execute("select content from content")
 
 result = mycursor.fetchall()
 for x in result:
-    getToken.tokenize(str(x[0]))
+    tokens = getToken.tokenize(str(x[0]))
+    tokens1 = {
+    "token": tokens["words1"],
+    "length": 1
+    }
+    print(tokens1)
+    tokens2 = {
+    "token": tokens["words2"],
+    "length": 2
+    }
+    tokens3 = {
+    "token": tokens["words3"],
+    "length": 3
+    }
+
+# tao http request
